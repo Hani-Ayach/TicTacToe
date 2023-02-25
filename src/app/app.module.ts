2b159f1632879c2760f4d5457f5ea8be320d1196
Hani Ayach <hani.rahif.ayach@gmail.com>
2023-02-25 22:50:25 +0200

    almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,41 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbButtonModule, NbCardComponent, NbCardModule } from '@nebular/theme';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbCardComponent,
+  NbCardModule,
+} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from 'src/environments/environment.prod';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
-  declarations: [
-    AppComponent,
-    SquareComponent,
-    BoardComponent
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    HttpClientModule,
+    TranslateModule.forRoot(
+     { loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+    }}
+    ),
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
     NbEvaIconsModule,
@@ -30,10 +45,12 @@ import { environment } from 'src/environments/environment.prod';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HttpClient
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,8 +1,5 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {
-  timeInterval,
-  TimeInterval,
-} from 'rxjs/internal/operators/timeInterval';
 
 @Component({
   selector: 'app-board',
@@ -13,10 +10,22 @@ export class BoardComponent implements OnInit {
   squares!: any[];
   xIsNext!: boolean;
   winner!: string;
-  constructor() {}
+  isEnglish:boolean=true;
+  lang:string='en';
+  constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
     this.newGame();
+
+    //lang setting 
+    console.log(this.isEnglish)
+    this.lang=localStorage.getItem('lang')||'en';
+    if(this.lang=='ar')
+    this.isEnglish=false;
+    else
+    this.isEnglish=true;
+
+    
   }
   newGame() {
     this.squares = Array(9).fill(null);
@@ -34,9 +43,6 @@ export class BoardComponent implements OnInit {
     }
     this.winner = this.calculateWinner();
 
-    if (this.winner == 'X' || this.winner == 'Y') {
-      setInterval(() => this.newGame(), 3000);
-    }
   }
 
   calculateWinner(): any {
@@ -53,7 +59,7 @@ export class BoardComponent implements OnInit {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
-        this.squares[a] &&
+        !!this.squares[a] &&
         this.squares[a] == this.squares[b] &&
         this.squares[a] == this.squares[c]
       ) {
@@ -62,4 +68,16 @@ export class BoardComponent implements OnInit {
     }
     return null;
   }
+
+  //language setup
+  OnChangeLang(lang:'ar'|'en'){
+    if(lang=='ar')
+    this.isEnglish=false;
+    else
+    this.isEnglish=true;
+
+    localStorage.setItem('lang',lang);
+    window.location.reload();
+  }
+ 
 }
